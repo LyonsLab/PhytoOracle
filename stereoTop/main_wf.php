072fcd39d900ed7527d2b5f05ba49d0eab16b707
John Xu <zhxu73@email.arizona.edu>
2019-12-11 17:59:05 -0500

    add fieldmosaic and canopy_cover

--- stereoTop/main_wf.php
+++ stereoTop/main_wf.php
@@ -17,9 +17,11 @@
   $TIFS_DIR = "bin2tif_out/";
   $SOILMASK_DIR = "soil_mask_out/";
   $FIELDMOSAIC_DIR = "fieldmosaic_out/";
+  $CANOPYCOVER_DIR = "canopy_cover_out/";
 
   $MOSAIC_LIST_FILE = $FIELDMOSAIC_DIR . "filelist.txt";
-
+  $SENSOR = "stereoTop";
+  $MOSAIC_BOUNDS = "-111.9750963 33.0764953 -111.9747967 33.074485715";
   /*
   "METADATA_CLEANED_LIST": [<?php foreach ($data_file_list as &$data_set) :?> "<?=$CLEANED_META_DIR?>" + "<?=$data_set["UUID"]?>" + "_metadata_cleaned.json", <?php endforeach?>],
   "LEFT_SOILMASK_LIST": [<?php foreach ($data_file_list as &$data_set) :?> "<?=$SOILMASK_DIR?>" + "<?=$data_set["UUID"]?>" + "_left_mask.tif", <?php endforeach?>],
@@ -144,6 +146,49 @@
       + [<?php foreach ($data_file_list as &$data_set) :?> "<?=$SOILMASK_DIR?>" + "<?=$data_set["UUID"]?>" + "_right_mask.tif", <?php endforeach?>],
       "outputs": [
         "<?=$MOSAIC_LIST_FILE?>"
+      ]
+    },
+
+
+    {
+      # Generate field mosaic from soil mask TIFFs
+      "command": "singularity run -B $(pwd):/mnt --pwd /mnt docker://agpipeline/fieldmosaic:latest --result print --metadata ${METADATA} --working_space ${WORKING_SPACE} ${MOSAIC_LIST_FILE} ${SENSOR} \"${MOSAIC_BOUNDS}\"",
+      "environment": {
+        "MOSAIC_LIST_FILE": "<?=$MOSAIC_LIST_FILE?>",
+        "METADATA": "<?=$CLEANED_META_DIR?>" + "<?=$data_file_list[0]["UUID"]?>" + "_metadata_cleaned.json",
+        "WORKING_SPACE": "<?=$FIELDMOSAIC_DIR?>",
+        "SENSOR": "stereoTop",
+        "MOSAIC_BOUNDS": "<?=$MOSAIC_BOUNDS?>"
+      },
+      "inputs": [
+        "<?=$FIELDMOSAIC_DIR?>",
+        "<?=$MOSAIC_LIST_FILE?>",
+        "<?=$CLEANED_META_DIR?>" + "<?=$data_file_list[0]["UUID"]?>" + "_metadata_cleaned.json"
+      ],
+      "outputs": [
+        "<?=$FIELDMOSAIC_DIR?>" + "fullfield_mosaic.png",
+        "<?=$FIELDMOSAIC_DIR?>" + "fullfield_mosaic.png.aux.xml",
+        "<?=$FIELDMOSAIC_DIR?>" + "fullfield_mosaic.tif",
+        "<?=$FIELDMOSAIC_DIR?>" + "fullfield_mosaic.vrt",
+        "<?=$FIELDMOSAIC_DIR?>" + "fullfield_mosaic_10pct.tif",
+        "<?=$FIELDMOSAIC_DIR?>" + "fullfield_mosaic_thumb.tif"
+      ]
+    },
+    {
+      # Generate canopy cover from field mosaic
+      "comand": "mkdir -p ${WORKING_SPACE} && singularity run -B $(pwd):/mnt --pwd /mnt docker://agpipeline/canopycover:latest --debug --result print --metadata ${METADATA} --working_space ${WORKING_SPACE} ${CANOPY_COVER_INPUT_FILE}",
+      "environment": {
+        "METADATA": "<?=$CLEANED_META_DIR?>" + "<?=$data_file_list[0]["UUID"]?>" + "_metadata_cleaned.json",
+        "WORKING_SPACE": "<?=$CANOPYCOVER_DIR?>",
+        "CANOPY_COVER_INPUT_FILE": "<?=$FIELDMOSAIC_DIR?>" + "fullfield_mosaic.tif"
+      },
+      "inputs": [
+        "<?=$CLEANED_META_DIR?>" + "<?=$data_file_list[0]["UUID"]?>" + "_metadata_cleaned.json",
+        "<?=$FIELDMOSAIC_DIR?>" + "fullfield_mosaic.tif"
+      ],
+      "outputs": [
+        "<?=$CANOPYCOVER_DIR?>" + "canopycover.csv",
+        "<?=$CANOPYCOVER_DIR?>" + "canopycover_geostreams.csv"
       ]
     }
   ]
