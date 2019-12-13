f7fc4e3f5fcbf231f7deaf76e5872d2c3aad335e
John Xu <zhxu73@email.arizona.edu>
2019-12-13 12:08:59 -0700

    Remove 200 remote jobs limit

--- stereoTop/entrypoint.sh
+++ stereoTop/entrypoint.sh
@@ -2,6 +2,5 @@
 
 
 # -a advertise to catalog server
-# -J max 200 remote job
-makeflow -T wq --json main_workflow_phase1.json -a -N phyto_oracle-atmo -p 9123 -J 200 -dall -o dall.log $@
+makeflow -T wq --json main_workflow_phase1.json -a -N phyto_oracle-atmo -p 9123 -dall -o dall.log $@
 
