4f5d7314c08fff746e79c18897ea6b00bcdab1d0
Michele Cosi <cosimichele@vm142-26.cyverse.org>
2020-04-20 11:55:33 -0700

    cleaup & added meantemp intermediate

--- FlirIr/entrypoint.sh
+++ FlirIr/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Phase 1
-python3 gen_files_list.py 2020-01-16 > raw_data_files.json
+python3 gen_files_list.py 2020-03-03 > raw_data_files.json
 python3 gen_bundles_list.py raw_data_files.json bundle_list.json 2
 mkdir -p bundle/
 python3 split_bundle_list.py  bundle_list.json bundle/
@@ -29,6 +29,9 @@ awk -F, '{print $4,$5,$1,$2,$3}' OFS=, t1.csv > t2.csv
 sed 's/^.\{9\}//' t2.csv > meantemp.csv
 sed -i '1 i\Plot, Temperature, Latitute, Longitude, Time' meantemp.csv
 
-# Cleanup ***REMOVE THIS TO SEE MEANTEMP INTERMEDIATE***
-rm -r t* meantemp_out/*
+# Cleanup 
+rm -r t*
+mkdir meantemp_intermediate/ && chmod 755 meantemp_intermediate/
+mv meantemp_out/* meantemp_intermediate/
+mv meantemp_intermediate/ meantemp_out/
 mv meantemp.csv meantemp_out/
