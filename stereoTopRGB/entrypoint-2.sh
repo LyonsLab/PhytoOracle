ac20015d77a01608a5a8e011a6c99a2fa88d9474
Emmanuel Miguel Gonzalez <emmanuelgonzalez@r1u04n2.puma.hpc.arizona.edu>
2020-09-02 11:34:21 -0700

    v2

--- stereoTopRGB/entrypoint-2.sh
+++ stereoTopRGB/entrypoint-2.sh
@@ -4,9 +4,9 @@
 #python3 gen_bundles_list.py raw_data_files.json bundle_list.json 6
 #mkdir -p bundle/
 #python3 split_bundle_list.py bundle_list.json bundle/
-/home/u31/emmanuelgonzalez/cctools-7.1.6-x86_64-centos7/bin/jx2json main_wf_phase-2.jx -a bundle_list.json > main_workflow_phase2.json
+${HOME}/cctools-7.1.6-x86_64-centos7/bin/jx2json main_wf_phase-2.jx -a bundle_list.json > main_workflow_phase2.json
 
 
 # -a advertise to catalog server
-/home/u31/emmanuelgonzalez/cctools-7.1.6-x86_64-centos7/bin/makeflow -T wq --json main_workflow_phase2.json -a -M phyto_oracle-atmo -N phyto_oracle-atmo -p 60221 -dall -o dall.log --disable-cache $@
+${HOME}/cctools-7.1.6-x86_64-centos7/bin/makeflow -T wq --json main_workflow_phase2.json -a -M phyto_oracle-atmo -N phyto_oracle-atmo -p 60221 -dall -o dall.log --disable-cache $@
 
