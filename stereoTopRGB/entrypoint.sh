60abc9a83bf0370064627b21186cdd005aa9d8d8
Emmanuel Miguel Gonzalez <emmanuelgonzalez@r1u04n2.puma.hpc.arizona.edu>
2020-09-02 11:29:38 -0700

    v2

--- stereoTopRGB/entrypoint.sh
+++ stereoTopRGB/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-python3 gen_files_list.py 2020-03-02 > raw_data_files.json
+python3 gen_files_list.py 2020-02-24 > raw_data_files.json
 python3 gen_bundles_list.py raw_data_files.json bundle_list.json 15
 mkdir -p bundle/
 python3 split_bundle_list.py bundle_list.json bundle/
-/home/u31/emmanuelgonzalez/cctools-7.1.6-x86_64-centos7/bin/jx2json main_wf_phase1.jx -a bundle_list.json > main_workflow_phase1.json
+${HOME}/cctools-7.1.6-x86_64-centos7/bin/jx2json main_wf_phase1.jx -a bundle_list.json > main_workflow_phase1.json
 
 
 # -a advertise to catalog server
-/home/u31/emmanuelgonzalez/cctools-7.1.6-x86_64-centos7/bin/makeflow -T wq --json main_workflow_phase1.json -a -N phyto_oracle-atmo -M phyto_oracle-atmo -p 60221 -dall -o dall.log --disable-cache $@
+${HOME}/cctools-7.1.6-x86_64-centos7/bin/makeflow -T wq --json main_workflow_phase1.json -a -N phyto_oracle-atmo -M phyto_oracle-atmo -p 60221 -dall -o dall.log --disable-cache $@
 
