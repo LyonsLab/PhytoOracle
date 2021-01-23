9ef918fdab9ba9a573893a8ff160f19f4466718b
Emmanuel Miguel Gonzalez <emmanuelgonzalez@junonia.hpc.arizona.edu>
2021-01-22 21:04:30 -0700

    updates

--- psII/worker_scripts/po_work_puma.sh
+++ psII/worker_scripts/po_work_puma.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
-#SBATCH --account=<account>
+#SBATCH --account=dukepauli
 #SBATCH --partition=standard
-#SBATCH --job-name="<name>"
+#SBATCH --job-name="po_worker"
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=94
+#SBATCH --ntasks-per-node=22
 #SBATCH --time=12:00:00
 #module load singularity
 module load python/3.8
@@ -12,4 +12,4 @@ module load python/3.8
 export CCTOOLS_HOME=${HOME}/cctools-7.1.6-x86_64-centos7
 export PATH=${CCTOOLS_HOME}/bin:$PATH
 
-${HOME}/cctools-7.1.6-x86_64-centos7/bin/work_queue_factory -T local -M phytooracle -w 60 -W 90 --workers-per-cycle 10 --cores=1 -t 900
+${HOME}/cctools-7.1.6-x86_64-centos7/bin/work_queue_factory -T local -M phytooracle -w 20 -W 20 --workers-per-cycle 10 --cores=1 -t 900
