1f652e7cb587e3c585b2abeaa5bfd60dc31bcb71
Emmanuel Miguel Gonzalez <emmanuelgonzalez@junonia.hpc.arizona.edu>
2021-01-29 11:55:58 -0700

    updated cctools

--- psII/worker_scripts/po_work_puma.sh
+++ psII/worker_scripts/po_work_puma.sh
@@ -4,12 +4,12 @@
 #SBATCH --job-name="po_worker"
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=22
+#SBATCH --ntasks-per-node=94
 #SBATCH --time=12:00:00
 #module load singularity
 module load python/3.8
 
-export CCTOOLS_HOME=${HOME}/cctools-7.1.6-x86_64-centos7
+export CCTOOLS_HOME=${HOME}/cctools-7.1.12-x86_64-centos7
 export PATH=${CCTOOLS_HOME}/bin:$PATH
 
-${HOME}/cctools-7.1.6-x86_64-centos7/bin/work_queue_factory -T local -M phytooracle -w 20 -W 20 --workers-per-cycle 10 --cores=1 -t 900
+${HOME}/cctools-7.1.12-x86_64-centos7/bin/work_queue_factory -T local -M phytooracle -w 90 -W 90 --workers-per-cycle 10 --cores=1 -t 900
--- psII/worker_scripts/worker_script.sh
+++ psII/worker_scripts/worker_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-export CCTOOLS_HOME=${HOME}/cctools-7.1.6-x86_64-centos7
+export CCTOOLS_HOME=${HOME}/cctools-7.1.12-x86_64-centos7
 export PATH=${CCTOOLS_HOME}/bin:$PATH
 
 #${HOME}/cctools-7.1.6-x86_64-centos7/bin/work_queue_worker -M phyto_oracle-atmo --cores 94 -t 259200
 #${HOME}/cctools-7.1.6-x86_64-centos7/bin/work_queue_factory -T local -M phyto_oracle-atmo -w 40 -W 90 --workers-per-cycle 10 --cores=1 -t 900
-${HOME}/cctools-7.1.6-x86_64-centos7/bin/slurm_submit_workers -p "--account=<account> --partition=standard --nodes=1 --ntasks=94 --cpus-per-task=1" -N phyto_oracle-atmo 94
+${HOME}/cctools-7.1.12-x86_64-centos7/bin/slurm_submit_workers -p "--account=<account> --partition=standard --nodes=1 --ntasks=94 --cpus-per-task=1" -N phyto_oracle-atmo 94
