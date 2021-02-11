53c33262fc793161fe72eb1e153bd6db3fb9251a
Emmanuel Miguel Gonzalez <emmanuelgonzalez@junonia.hpc.arizona.edu>
2021-02-11 08:54:42 -0700

    new worker script

--- psII/worker_scripts/po_work_puma_slurm.sh
+++ psII/worker_scripts/po_work_puma_slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH --account=<account>
-#SBATCH --partition=<windfall or standard>
+#SBATCH --partition=standard
 #SBATCH --job-name="po_worker"
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -12,4 +12,4 @@ module load python/3.8
 export CCTOOLS_HOME=${HOME}/cctools-7.1.12-x86_64-centos7
 export PATH=${CCTOOLS_HOME}/bin:$PATH
 
-${HOME}/cctools-7.1.12-x86_64-centos7/bin/work_queue_factory -T slurm -M phytooracle -w <min_workers> -W <max_workers> --workers-per-cycle 0 --cores=1 -t 300
+${HOME}/cctools-7.1.12-x86_64-centos7/bin/work_queue_factory -T slurm -B "--account=<account> --partition=standard" -M phytooracle -w <min_workers> -W <max_workers> --workers-per-cycle 0 --cores=1 -t 300
