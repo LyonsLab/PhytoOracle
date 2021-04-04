948814fa23e32efcb6e100231e75e2d43de4caa3
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-04-04 03:14:27 -0700

    updated worker scripts

--- scanner3DTop/worker_scripts/po_work_puma_slurm.sh
+++ scanner3DTop/worker_scripts/po_work_puma_slurm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
-#SBATCH --account=dukepauli
+#SBATCH --account=frost_lab
 #SBATCH --partition=standard
 #SBATCH --job-name="po_worker"
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=94
+#SBATCH --ntasks-per-node=1
 #SBATCH --time=12:00:00
 #module load singularity
 module load python/3.8
@@ -12,6 +12,4 @@ module load python/3.8
 export CCTOOLS_HOME=${HOME}/cctools-7.1.12-x86_64-centos7
 export PATH=${CCTOOLS_HOME}/bin:$PATH
 
-#cd /xdisk/ericlyons/big_data/egonzalez/phyto_training/PhytoOracle/stereoTopRGB/
-
-${HOME}/cctools-7.1.12-x86_64-centos7/bin/work_queue_factory -T local -M phytooracle -w 60 -W 80 --workers-per-cycle 10 --cores=1 -t 900
+${HOME}/cctools-7.1.12-x86_64-centos7/bin/work_queue_factory -T slurm -M phytooracle -B "--account=dukepauli --partition=standard"  -w 10 -W 20 --workers-per-cycle 0 --cores=16 -t 300
