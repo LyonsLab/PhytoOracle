999737461e7423a9a601c90b17d4f2b3b54f76ce
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-05-03 12:17:33 -0700

    season11 automation

--- stereoTopRGB/run.sh
+++ stereoTopRGB/run.sh
@@ -6,39 +6,40 @@ PLOTCLIP_DIR='plotclip_out/'
 PIPE_PATH=$PWD'/'
 SIMG_PATH='/xdisk/ericlyons/big_data/singularity_images/'
 OUT_PATH=${PIPE_PATH}'ortho_out/'
-#CSV_PATH=${PIPE_PATH}'img_coords_out/'${SCAN_DATE}'_coordinates.csv'
 set -e 
 
+#---------------------------------------------Download raw data
 sbatch worker_scripts/po_work_puma_slurm.sh
 singularity run ${SIMG_PATH}slack_notification.simg -m "Downloading and preparing to process stereoTop-${SCAN_DATE}." 
 echo "> Processing ${SCAN_DATE} RGB scan."
 ssh filexfer 'cd' "${PIPE_PATH}" '&& ./download.sh' ${SCAN_DATE} ${PIPE_PATH} '&& exit'
 
-# --------------------------------------------------
+# ---------------------------------------------Workflow 1
 echo "> Distributed workflow 1 of 2"
 ./replace.py "stereoTop-${SCAN_DATE}"
 ./replace_process_one.py $PWD
 ./entrypoint.sh
 
-# --------------------------------------------------
+# ---------------------------------------------Geocorrection
 echo "> Geocorrection 1 of 1"
 mkdir -p ortho_out/
 singularity exec ${SIMG_PATH}full_geocorrection.simg python3 $HOME/Lettuce_Image_Stitching/Dockerized_GPS_Correction_HPC.py -d ${OUT_PATH} -b ${PIPEPATH}bin2tif_out -s ${SCAN_DATE} -c $HOME/Lettuce_Image_Stitching/geo_correction_config.txt -l ${PIPE_PATH}gcp_season_11.txt -r $HOME/Lettuce_Image_Stitching
 
-# --------------------------------------------------
+# ---------------------------------------------Workflow 2
 echo "> Distributed workflow 2 of 2"
 ./entrypoint-2.sh 
 
-# --------------------------------------------------
+# ---------------------------------------------Deploy detection
 echo "> Generating orthomosaics and detecting plants."
+cancel --name=po_worker
 ls *_plotclip.tar | xargs -I {} tar -xvf {}
 rm *_plotclip.tar
 singularity run ${SIMG_PATH}rgb_flir_stitch_plots.simg -o ${SCAN_DATE}_plotclip_orthos ${PLOTCLIP_DIR}
 singularity exec ${SIMG_PATH}gdal_313.simg bash ortho.sh gpscorrect_out ${SCAN_DATE}
-singularity run ${SIMG_PATH}rgb_flir_plant_detection.simg -d ${SCAN_DATE} -m ${PIPE_PATH}model_weights_sorghum_rgb.pth -g ${PIPE_PATH}season11_multi_latlon_geno.geojson -t RGB -o season11_plant_detection ${SCAN_DATE}_plotclip_orthos 
+singularity run ${SIMG_PATH}rgb_flir_plant_detection.simg -d ${SCAN_DATE} -m ${PIPE_PATH}model_weights_sorghum_rgb.pth -g ${PIPE_PATH}season11_multi_latlon_geno.geojson -c plant -t RGB -o season11_plant_detection ${SCAN_DATE}_plotclip_orthos 
 
-# --------------------------------------------------
-singularity run ${SIMG_PATH}slack_notification.simg -m "Finished processing ${SCAN_DATE}. Now compressing outputs."
+# ---------------------------------------------Compress output
+singularity run ${SIMG_PATH}slack_notification.simg -m "Finished processing stereoTop-${SCAN_DATE}. Now compressing outputs."
 echo "> Compressing outputs."
 tar -cvf ${SCAN_DATE}_bin2tif.tar bin2tif_out/
 tar -cvf ${SCAN_DATE}_gpscorrect.tar gpscorrect_out/
@@ -48,21 +49,18 @@ cp raw_data_files.json bundle/
 tar -cvf ${SCAN_DATE}_bundle.tar bundle/
 tar -cvf ${SCAN_DATE}_plotclip_orthos.tar ${SCAN_DATE}_plotclip_orthos/ 
 rm -r ${SCAN_DATE}_plotclip_orthos/
-mv ${SCAN_DATE} processed_scans/
+rm -r stereoTop-${SCAN_DATE}
 mkdir -p ${SCAN_DATE}
 mv ${SCAN_DATE}_* ${SCAN_DATE}/
 ./clean.sh
+singularity run ${SIMG_PATH}slack_notification.simg -m "Finished compressing stereoTop-${SCAN_DATE}. Now uploading to the CyVerse DataStore."
 
-singularity run ${SIMG_PATH}slack_notification.simg -m "Finished compressing ${SCAN_DATE}. Now uploading to the CyVerse DataStore."
-#Upload outputs
+#-----------------------------------------------Upload outputs
 ssh filexfer 'cd' "${PIPE_PATH}" '&& ./upload.sh' ${SCAN_DATE} ${PIPE_PATH} '&& exit'
 
-#Cancel workers and clean the file system.
+#-----------------------------------------------Cancel workers and clean the file system.
 singularity run ${SIMG_PATH}slack_notification.simg -m "Upload complete. See outputs at /iplant/home/shared/phytooracle/season_11_sorghum_yr_2020/level_0/stereoTop/${SCAN_DATE}/"
-
-scancel --name=po_worker
 rm -r ${SCAN_DATE}
-rm -r processed_scans/
  
-# --------------------------------------------------
+# ----------------------------------------------End
 echo "> Done processing ${SCAN_DATE} RGB scan."
