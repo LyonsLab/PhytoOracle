89d96d5f83e3f8ae5b1eb484a5340b123131c7c0
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2021-05-24 01:23:56 -0700

    clean

--- psII/entrypoint.sh
+++ psII/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 gen_files_list.py 2020-09-11 RAW_DATA_PATH _rawData0042.bin > raw_data_files.json
+python3 gen_files_list.py 2020-09-11_sub1 RAW_DATA_PATH _rawData0042.bin > raw_data_files.json
 python3 gen_bundles_list.py raw_data_files.json bundle_list.json 1
 mkdir -p bundle/
 python3 split_bundle_list.py bundle_list.json bundle/
