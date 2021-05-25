d812a8133e124de621ef7cc5603b4f5eabae815c
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2021-05-24 21:30:11 -0700

    local wf

--- FlirIr/entrypoint.sh
+++ FlirIr/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-python3 gen_files_list.py flirIrCamera-2020-09-15__12-25-49-034 > raw_data_files.json
+python3 gen_files_list.py 2020-09-11 > raw_data_files.json
 python3 gen_bundles_list.py raw_data_files.json bundle_list.json 2
 mkdir -p bundle/
 python3 split_bundle_list.py  bundle_list.json bundle/
