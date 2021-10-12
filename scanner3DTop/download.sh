d1276665bf223d79ebebd6b8442cebb3da2c0379
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-10-12 07:27:04 -0700

    new pipeline

--- scanner3DTop/download.sh
+++ scanner3DTop/download.sh
@@ -3,7 +3,7 @@ TAR_FILE=${1}
 WORK_DIR=${2}
 
 cd ${WORK_DIR}
-iget -fKPVT /iplant/home/shared/phytooracle/season_10_lettuce_yr_2020/level_0/scanner3DTop/${TAR_FILE}.tar
-tar -xvf ${TAR_FILE}.tar
-rm ${TAR_FILE}.tar
-mv scanner3DTop/* .
+iget -fKPVT /iplant/home/shared/phytooracle/season_10_lettuce_yr_2020/level_0/scanner3DTop/${TAR_FILE}.tar.gz
+tar -xzvf ${TAR_FILE}.tar.gz
+rm ${TAR_FILE}.tar.gz
+#mv scanner3DTop/* .
--- scanner3DTop/worker_scripts/po_work_puma_slurm.sh
+++ scanner3DTop/worker_scripts/po_work_puma_slurm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --account=frost_lab
+#SBATCH --account=lyons-lab
 #SBATCH --partition=standard
 #SBATCH --job-name="po_worker"
 #SBATCH --nodes=1
@@ -10,4 +10,4 @@
 export CCTOOLS_HOME=${HOME}/cctools-7.1.12-x86_64-centos7
 export PATH=${CCTOOLS_HOME}/bin:$PATH
 
-${HOME}/cctools-7.1.12-x86_64-centos7/bin/work_queue_factory -T slurm -B "--account=frost_lab --partition=standard --job-name=po_worker" -M phytooracle_3d -w 10 -W 20 --workers-per-cycle 0 --cores=16 -t 300
+${HOME}/cctools-7.1.12-x86_64-centos7/bin/work_queue_factory -T slurm -B "--account=lyons-lab --partition=standard --job-name=po_worker --time=24:00:00" -M phytooracle_3d -w 50 -W 110 --workers-per-cycle 0 --cores=4 -t 300
