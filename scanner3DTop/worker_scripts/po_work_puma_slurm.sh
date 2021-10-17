81ae1fb7a96a8e853c31aed1925adc212b545c9b
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-10-17 16:58:36 -0700

    new pipeline

--- scanner3DTop/worker_scripts/po_work_puma_slurm.sh
+++ scanner3DTop/worker_scripts/po_work_puma_slurm.sh
@@ -10,4 +10,4 @@
 export CCTOOLS_HOME=${HOME}/cctools-7.1.12-x86_64-centos7
 export PATH=${CCTOOLS_HOME}/bin:$PATH
 
-${HOME}/cctools-7.1.12-x86_64-centos7/bin/work_queue_factory -T slurm -B "--account=lyons-lab --partition=standard --job-name=po_worker --time=24:00:00" -M phytooracle_3d -w 50 -W 110 --workers-per-cycle 0 --cores=4 -t 300
+${HOME}/cctools-7.1.12-x86_64-centos7/bin/work_queue_factory -T slurm -B "--account=lyons-lab --partition=standard --job-name=po_worker --time=24:00:00" -M phytooracle_3d -w 500 -W 1100 --workers-per-cycle 0 --cores=8 -t 300
