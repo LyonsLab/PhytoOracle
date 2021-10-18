ed2dc62f176c19757dc024f652f8976a368909eb
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-10-17 22:12:13 -0700

    new pipeline

--- scanner3DTop/build_containers.sh
+++ scanner3DTop/build_containers.sh
@@ -1,8 +1,6 @@
 #!/bin/bash 
 
-singularity build 3d_icp_merge.simg docker://phytooracle/3d_icp_merge:latest
-singularity build 3d_geo_ref.simg docker://phytooracle/3d_geo_registration:latest
-#singularity build 3d_geo_correction.simg docker://phytooracle/3d_geo_correction:latest
-#singularity build 3d_plant_clip.simg docker://phytooracle/3d_plant_clip:latest
-singularity build 3d_down_sample.simg docker://phytooracle/3d_down_sample:latest 
-singularity build 3d_heat_map.simg docker://phytooracle/3d_heat_map:latest 
+singularity build gantry_notifications.simg docker://phytooracle/slack_notifications:latest
+singularity build 3d_preprocessing.simg docker://phytooracle/3d_preprocessing:latest
+singularity build 3d_sequential_align.simg docker://phytooracle/3d_sequential_align:latest
+
--- scanner3DTop/run.sh
+++ scanner3DTop/run.sh
@@ -1,28 +1,28 @@
 #!/bin/bash 
 SCAN_DATE=${1%/}
 PIPE_PATH=$PWD'/'
+CHANNEL='gantry_test'
 
-#singularity run ${PIPE_PATH}gantry_notifications.simg -m "Processing ${SCAN_DATE}" -c "gantry_data_updates"
+# Prepare data
+singularity run ${PIPE_PATH}gantry_notifications.simg -m "Downloading ${SCAN_DATE}" -c ${CHANNEL}
 sbatch worker_scripts/po_work_puma_slurm.sh
 echo "> Processing ${SCAN_DATE} 3D scan."
 ssh filexfer 'cd' "${PIPE_PATH}" '&& ./download.sh' ${SCAN_DATE} ${PIPE_PATH} '&& exit'
-
 #SCAN_DATE="`echo ${SCAN_DATE} | grep -Eo '[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}' | tail -1`"
 
-# Set paths
 ./replace.py ${SCAN_DATE}
 ./replace_process_one.py $PWD
 
-# Run pipeline
+# Run pipeline workflow #1
+singularity run ${PIPE_PATH}gantry_notifications.simg -m "Processing ${SCAN_DATE}" -c ${CHANNEL}
 ./entrypoint_p1.sh
-#singularity run ${PIPE_PATH}gantry_notifications.simg -m "Finished processing ${SCAN_DATE}. Compressing outputs." -c "gantry_data_updates"
-
 singularity run ${PIPE_PATH}3d_sequential_align.simg -i preprocessing_out/ -o sequential_alignment_out/
 
-# Run pipeline 2
+# Run pipeline workflow #2
 #./entrypoint_p2.sh
 
 # Compress outputs
+singularity run ${PIPE_PATH}gantry_notifications.simg -m "Compressing ${SCAN_DATE} outputs" -c ${CHANNEL}
 mkdir -p processed_scans/ 
 mv ${SCAN_DATE} processed_scans/
 mkdir ${SCAN_DATE}
@@ -91,11 +91,11 @@ mv bundle_list.json ${PIPE_PATH}${SCAN_DATE}
 #./clean.sh
 
 #Upload outputs
-#singularity run ${PIPE_PATH}gantry_notifications.simg -m "Finished compressing scanner3DTop-${SCAN_DATE}. Now uploading to CyVerse." -c "gantry_data_updates"
+singularity run ${PIPE_PATH}gantry_notifications.simg -m "Finished compressing ${SCAN_DATE}. Now uploading to CyVerse." -c ${CHANNEL}
 ssh filexfer 'cd' "${PIPE_PATH}" '&& ./upload.sh' ${SCAN_DATE} ${PIPE_PATH} '&& exit'
 
 #Cancel workers and clean the file system.
-#singularity run ${PIPE_PATH}gantry_notifications.simg -m "Upload complete. See ouputs at /iplant/home/shared/phytooracle/season_10_lettuce_yr_2020/level_1/scanner3DTop/${SCAN_DATE}" -c "gantry_data_updates"
+singularity run ${PIPE_PATH}gantry_notifications.simg -m "Upload complete. See ouputs at /iplant/home/shared/phytooracle/season_10_lettuce_yr_2020/level_1/scanner3DTop/${SCAN_DATE}" -c ${CHANNEL}
 scancel --name=po_worker
 rm -r ${SCAN_DATE}
 rm -r processed_scans/${SCAN_DATE}
