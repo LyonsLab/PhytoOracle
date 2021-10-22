89ede8ede388fee8e8518cadecdc1d5bf43b3d76
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-10-22 15:45:16 -0700

    new pipeline

--- scanner3DTop/upload.sh
+++ scanner3DTop/upload.sh
@@ -8,7 +8,7 @@ cd ${WORK_DIR}
 icd /iplant/home/shared/phytooracle/season_10_lettuce_yr_2020/level_1/scanner3DTop
 #icd /iplant/home/shared/phytooracle/season_10_lettuce_yr_2020/level_1/scanner3DTop/${TAR_FILE}
 
-iput -rKPVT ${TAR_FILE}
+iput -rfKPVT ${TAR_FILE}
 ./clean.sh
 #iput -rKPVT alignment 
 #iput -rKPVT preprocessing
